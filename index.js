26a2fcbd394ba9225b0092a75d6d69e714de14b0
unknown <malailiyati107@gmail.com>
2025-07-14 09:00:35 +0700

    feat: import dan pemanggilan fungsi

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 //import fungsi
-const fetchData = require("./src/task1.js");
+const { fetchData, getData } = require("./src/task1.js");
 const { getDataFromServer, processData } = require("./src/task2.js");
 const getNameAndDomisili = require("./src/task3.js");
 const divideAndSort = require("./src/task4.js");
 const main = require("./src/task5.js");
 
-fetchData.getData();
+//pemanggilan fungsi
+fetchData(true)
+  .then((status) => {
+    console.log(status);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+getData();
 
 getDataFromServer(true, processData);
 
