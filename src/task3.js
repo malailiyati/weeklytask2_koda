2d019aedeef06bdd1261da7e7d20828805917ece
unknown <malailiyati107@gmail.com>
2025-07-14 10:54:54 +0700

    feat: menghapus localCompare dan hanya menggunakan .sort

--- src/task3.js
+++ src/task3.js
@@ -6,7 +6,7 @@ async function getNameAndDomisili() {
     const response = await fetch(url); //mengambil data dari API
     if (!response.ok) throw new Error(response.status); //kalau respon gagal lempar ke error
     const getData = await response.json(); //ubah response menjadi json
-    getData.sort((a, b) => a.address.city.localeCompare(b.address.city)); //mengurutkan data berdasarkan nama kota
+    getData.sort((a, b) => (a.address.city < b.address.city ? -1 : 1)); //mengurutkan data berdasarkan nama kota
     const result = await Promise.all(
       //mappingg
       getData.map(async (data) => {
@@ -23,4 +23,4 @@ async function getNameAndDomisili() {
   }
 }
 
-module.exports = getNameAndDomisili; //eksport
+module.exports = getNameAndDomisili;
